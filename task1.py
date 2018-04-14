e5a598eb52d0d656a5779a03c55831e2ea69b2f5
Natalia Ekimova <37003488+Natalieki@users.noreply.github.com>
2018-04-14 11:32:09 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,10 @@
-import math
-pi = 3.14
+from math import pi
+
 def calculate (R, L):
 	volume = pi * R * R * L
-
-	return ('Volume: ' + str(volume))
-
-
-
+	return ('Volume of cylinder is: ' + str(volume))
 R = int(input('Please Enter the Radius of Cylinder: '))
 L = int(input('Please Enter the Height of Celinder: '))
-
+print ("pi=")
+print (pi)
 print (calculate(R, L))
-
