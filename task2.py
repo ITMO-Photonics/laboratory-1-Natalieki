a2fb405ec0c8ae7b0f85a65fb8f4713f570a4211
Natalia Ekimova <37003488+Natalieki@users.noreply.github.com>
2018-04-14 11:32:41 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,6 @@
 import timeit
 import numpy as np
 import scipy.optimize as optimize
-
 import time
 
 def f(x): 
@@ -10,20 +9,24 @@ def f(x):
 def fprime(x):
     return (-(x**2+1.)*np.sin(x)-2.*x*np.cos(x))/(x**2+1)**2
 
+print ("time of calculations:")
 
+# метод деления отрезка
+start_time = time.time()
+bisect_x = optimize.bisect(f, 0.1, 2.4)
+print("bisect - %s sec" % (time.time() - start_time))
+
+# метод Ньютона с заданием производной
+start_time = time.time()
+newton_x = optimize.newton(f, 0.1, fprime)
+print("newton - %s sec" % (time.time() - start_time))
+
+# метод секущих
+start_time = time.time()
+secant_x = optimize.newton(f, 0.1, 2.4)
+print("secant - %s sec" % (time.time() - start_time))
+
+# метод Брента
 start_time = time.time()
 brentq_x = optimize.brentq(f, 0.1, 2.4)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-bisect_x = optimize.bisect(f, 0.1, 2.4)
-newton_x = optimize.newton(f, 0.1)
-newtonx2_x = optimize.newton(f, 0.1, fprime)
-
-bisect_time = optimize.bisect (f, 0.1, 2.4)
-print("bisect", bisect_time)
-brentq_time = optimize.brentq (f, 0.1, 2.4)
-print("brentq", brentq_time)
-newton_time = optimize.newton (f, 0.1)
-print("newton", newton_time)
-newtonx2_time = optimize.newton (f, 0.1, fprime)
-print("newtonx2", newtonx2_time)
+print("brentq - %s sec" % (time.time() - start_time))
