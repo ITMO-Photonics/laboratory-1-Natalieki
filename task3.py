592544b9d00331dbc2dec0cf5caef08c0fd6dc36
Natalia Ekimova <37003488+Natalieki@users.noreply.github.com>
2018-04-14 11:56:28 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,15 +1,15 @@
 import scipy.linalg as sl
 import numpy as np
-N=100
-A=np.zeros((N,N))
-B=np.zeros((N))
-i,j=np.indices(A.shape)
-for k in range(int(N)):
-    A[i==j+k]=1
-    B[k]=k
-print('A - matrix:')
-print(A)
-print('B - vector:')
-print(B)
-print('Answer vector:')
-print(sl.solve(A,B))
+
+N=20
+A=np.zeros((N,N)) 
+i,j=np.indices(A.shape) 
+
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+
+B=np.arange(N)
+C=np.linalg.solve(A,B)
+
+print (C);
