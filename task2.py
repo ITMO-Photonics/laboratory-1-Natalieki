62b23627a24ff618b9288edb24e176035e24d283
Natalia Ekimova <37003488+Natalieki@users.noreply.github.com>
2018-05-12 12:45:43 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -13,20 +13,24 @@ print ("time of calculations:")
 
 # метод деления отрезка
 start_time = time.time()
-bisect_x = optimize.bisect(f, 0.1, 2.4)
+for i in  range (100):
+	bisect_x = optimize.bisect(f, 0.1, 2.4)
 print("bisect - %s sec" % (time.time() - start_time))
 
 # метод Ньютона с заданием производной
 start_time = time.time()
-newton_x = optimize.newton(f, 0.1, fprime)
+for i in  range (100):
+	newton_x = optimize.newton(f, 1.2, fprime)
 print("newton - %s sec" % (time.time() - start_time))
 
 # метод секущих
 start_time = time.time()
-secant_x = optimize.newton(f, 0.1, 2.4)
+for i in  range (100):
+	secant_x = optimize.newton(f, 1.2)
 print("secant - %s sec" % (time.time() - start_time))
 
 # метод Брента
 start_time = time.time()
-brentq_x = optimize.brentq(f, 0.1, 2.4)
+for i in  range (100):
+	brentq_x = optimize.brentq(f, 0.1, 2.4)
 print("brentq - %s sec" % (time.time() - start_time))
